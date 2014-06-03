2484dfb13c751597f3fdcd9535cede85b287cdc2
s1180040 <s1180040@gmail.com>
2014-06-03 15:19:35 +0900

    コメントを追加
    各図形の大きさを大きく

--- src/main/scala/org/zukkey40/example/scalafx/shape/ShapeExampleMain.scala
+++ src/main/scala/org/zukkey40/example/scalafx/shape/ShapeExampleMain.scala
@@ -14,42 +14,47 @@ import scalafx.geometry.Pos
  * Created with IntelliJ IDEA.
  * User: zuk_key
  * Date: 2014/06/01
- * To change this template use File | Settings | File Templates.
+ *
+ * 長方形、円、折れ線を表示するサンプルになります。
  */
-object Main extends JFXApp{
+object ShapeExampleMain extends JFXApp{
+  /**
+   * グリッド状の枠組みからなるレイアウトをルートとしています。
+   */
   val root: GridPane = new GridPane{
-    hgap = 10.0
-    vgap = 10.0
+    hgap = 15.0
+    vgap = 15.0
     alignment = Pos.CENTER
   }
 
   val rectangle: Rectangle = new Rectangle{
-    height = 50
-    width = 50
+    height = 70
+    width = 70
     stroke = Color.BLACK
     fill = Color.YELLOW
   }
-  root.add(rectangle, 0, 0) //(x, y) = (0, 0)の位置に追加する
+  root.add(rectangle, 0, 0) //(x, y) = (0, 0)の位置に、四角形を追加する
 
   val circle: Circle = new Circle{
-    radius = 20.0
-    stroke = Color.BLACK
+    radius = 35.0
+    stroke = Color.DARKBLUE
     fill = Color.LIGHTBLUE
   }
-  root.add(circle, 1, 0) //(1, 0)の位置に追加する
+  root.add(circle, 1, 0) //(1, 0)の位置に、円を追加する
 
   val trapezoid: Polyline = new Polyline{
     points += (
       0.0, 0.0,
-      50.0, 0.0,
-      40.0, 30.0,
-      10.0, 30.0,
+      90.0, 0.0,
+      80.0, 50.0,
+      10.0, 50.0,
       0.0, 0.0
       )
     fill = Color.VIOLET
+    stroke = Color.DARKRED
     alignmentInParent = Pos.CENTER
   }
-  root.add(trapezoid, 0, 1, 2, 1) //(0, 1)~(1,1)の位置に追加する
+  root.add(trapezoid, 0, 1, 2, 1) //(0, 1)~(1,1)の位置に、折れ線（台形）を追加する
 
   stage = new PrimaryStage() {
     title = "FXML example"
